c9534979ddb06205773574e296b569b522e8db1d
mumo-esther <mumoesther2021@gmail.com>
2023-03-30 21:53:08 +0300

    Added Routing pages

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,14 +1,26 @@
-import Header from '@/components/Header';
-import TodosLogic from '@/components/TodosLogic';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from '@/pages/Home';
+import About from '@/pages/About';
+import Login from '@/pages/Login';
+import Profile from '@/pages/Profile';
+import NotMatch from '@/pages/NotMatch';
+import Layout from '@/components/Layout';
+import SinglePage from '@/pages/SinglePage';
 
 const TodoApp = () => {
   return (
-    <div className="wrapper">
-      <div className="todos">
-        <Header />
-        <TodosLogic />
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />}>
+          <Route path=":slug" element={<SinglePage />} />
+        </Route>
+      <Route path="login" element={<Login />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="*" element={<NotMatch />} />
+      </Route>
+    </Routes>
   );
 };
 export default TodoApp;
